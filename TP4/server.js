e6d2cf554762d7496e6d1b469efe91026268c783
PMimosoS <38466667+PMimosoS@users.noreply.github.com>
2018-10-15 01:05:27 +0100

    Add files via upload

---
+++ TP4/server.js
@@ -0,0 +1,32 @@
+var app = require('http').createServer(createServer);
+var fs = require('fs'); 
+var url = require('url');
+
+function createServer(req, res) {
+    var q =url.parse(req.url,true)
+    var number = q.pathname.substring(6)
+    var path = url.parse(req.url).pathname;
+    var fsCallback = function(erro, dados) {
+        if(!erro)
+            res.write(dados)
+        else
+            res.write(erro)
+            res.end() 
+    }
+
+    switch(path) {
+        case '/obram'+number:
+            console.log("Obra:"+ number)
+            fs.readFile(__dirname + '/website/html/obram'+ number +'.html', fsCallback);
+        break;
+        case '/index':
+            console.log("Index")
+            fs.readFile(__dirname + '/website/index.html', fsCallback);   
+        break;
+        default:
+            fs.readFile(__dirname + '/website/index.html', fsCallback);
+    }
+}
+
+app.listen(7777);
+console.log("Servidor na porta 7777")
